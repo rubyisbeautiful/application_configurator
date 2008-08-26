dc6fffe7443a4838229893c9e14b1e2eb09adec3
Roman <roman@linux-desktop.(none)>
2008-08-26 09:01:58 +0300

    one more blank? check

--- generators/config_item/templates/_children_html_haml.rb
+++ generators/config_item/templates/_children_html_haml.rb
@@ -2,7 +2,7 @@
   = @config_item.param_name
 - children.each do |config_item|
   %ul
-    - if config_item.param_value.nil?
+    - if config_item.param_value.blank?
       %li{:id => "config_item_#{config_item.id}"}
         = link_to_remote config_item.param_name, :url => children_config_item_path(config_item), :update => "config_item_#{config_item.id}", :method => :get
     - else
